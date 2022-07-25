885dc59cea4d91471afe40589ddb8490df633c09
Petar <zafirovskipetar@gmail.com>
2022-07-25 17:55:58 +0200

    initial commit

---
+++ Tasks/src/test/java/mk/ukim/finki/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
