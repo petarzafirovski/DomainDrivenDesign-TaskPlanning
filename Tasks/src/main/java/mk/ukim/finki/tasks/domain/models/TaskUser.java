06c8e77fd5b04e64c54414e3cfe3f20142b34c1a
SaraMisajlovska <smisajlovska@gmail.com>
2022-08-05 12:03:45 +0200

    added OneToMany relation in TaskUser for tasks

--- Tasks/src/main/java/mk/ukim/finki/tasks/domain/models/TaskUser.java
+++ Tasks/src/main/java/mk/ukim/finki/tasks/domain/models/TaskUser.java
@@ -1,20 +1,29 @@
 package mk.ukim.finki.tasks.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NonNull;
 import mk.ukim.finki.sharedkernel.domain.base.AbstractEntity;
 import mk.ukim.finki.sharedkernel.domain.base.DomainObjectId;
 import mk.ukim.finki.tasks.domain.valueobjects.UserId;
+import mk.ukim.finki.users.domain.model.TaskItem;
 
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "task_user")
 @Getter
 public class TaskUser extends AbstractEntity<TaskUserId> {
+
     @AttributeOverride(name = "id", column = @Column(name = "task_user_id", nullable = false))
     private UserId userId;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Task> tasks = new HashSet<>();
 
     public TaskUser(@NonNull UserId userId) {
         super(DomainObjectId.randomId(TaskUserId.class));
