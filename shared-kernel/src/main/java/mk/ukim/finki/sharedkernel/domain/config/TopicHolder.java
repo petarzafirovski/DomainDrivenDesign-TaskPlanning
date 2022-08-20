025d09e135d223b25f4ecb00c7276580bcf94e26
Petar <zafirovskipetar@gmail.com>
2022-08-20 17:09:06 +0200

    added topic holder for kafka events

---
+++ shared-kernel/src/main/java/mk/ukim/finki/sharedkernel/domain/config/TopicHolder.java
@@ -0,0 +1,8 @@
+package mk.ukim.finki.sharedkernel.domain.config;
+
+public class TopicHolder {
+    public final static String TOPIC_TASK_CREATED = "task-created";
+    public final static String TOPIC_TASK_UPDATED = "task-updated";
+    public final static String TOPIC_TASK_DELETED = "task-deleted";
+    public final static String TOPIC_TASK_FINDALLBYUSER = "task-findAllByUser";
+}
